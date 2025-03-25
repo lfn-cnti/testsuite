34d31b6e4e0ab5c17de6f0291b941307f56a8a49
Rafal Lal <rafal.lal@tietoevry.com>
2025-03-25 12:27:37 +0000

    Add deprecated K8s features test
    Test if CNF deployment (be it helm or manifests based) contains any
    deprecated features. Check is performed by collecting stderr from
    'helm install' and 'kubectl apply' commands. These have the latest and
    most reliable info regarding deprecations. 3rd-party tools like 'kubent'
    or 'pluto' were not succesful in discovering all deprecations and would
    add another dependency to long list of already maintained.
    
    - Add deprecation test
    - Add spec for test checking all 3 possible outcomes
    - Add error handling to manifest_deployment_manager.install()
    - Add stderr collection to logfile in deployment_managers
    - Update exceptions raised in helm module
    - Run autoformatter on compatibility_spec.cr
    - Update kind in GH actions to latest v0.27.0 which uses k8s 1.32.0
    
    Signed-off-by: Rafal Lal <rafal.lal@tietoevry.com>

--- src/tasks/utils/cnf_manager/cnf_manager.cr
+++ src/tasks/utils/cnf_manager/cnf_manager.cr
@@ -132,6 +132,7 @@ module CNFManager
     !cnf_config_list(false).empty?
   end
 
+  # (rafal-lal) TODO: why are we not accepting *.yaml
   def self.path_has_yml?(config_path)
     config_path =~ /\.yml/
   end
