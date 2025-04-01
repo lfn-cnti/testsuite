a9475e53447ff0ded2851cfe9610951d10aa593a
Rafal Lal <rafal.lal@tietoevry.com>
2025-04-01 13:23:46 +0000

    Refactor OPA setup task
    Signed-off-by: Rafal Lal <rafal.lal@tietoevry.com>

--- src/tasks/setup/opa_setup.cr
+++ src/tasks/setup/opa_setup.cr
@@ -1,42 +1,86 @@
 require "sam"
-require "file_utils"
-require "colorize"
-require "totem"
-require "./utils/utils.cr"
+require "../utils/utils.cr"
 
-desc "Sets up OPA in the K8s Cluster"
-task "install_opa", ["helm_local_install", "create_namespace"] do |_, args|
-  helm_install_args_list = [
-    "--set auditInterval=1",
-    "--set postInstall.labelNamespace.enabled=false",
-    "-n #{TESTSUITE_NAMESPACE}"
-  ]
+namespace "setup" do
+  desc "Sets up OPA in the K8s Cluster"
+  task "install_opa", ["helm_local_install", "create_namespace"] do |_, args|
+    logger = SLOG.for("install_opa")
+    logger.info { "Installing Open Policy Agent tool" }
+    failed_msg = "Task 'install_opa' failed"
 
-  k8s_server_version = KubectlClient.server_version
-  if !version_less_than(k8s_server_version, "1.25.0")
-    helm_install_args_list.push("--set psp.enabled=false")
+    helm_install_args_list = [
+      "--set auditInterval=1",
+      "--set postInstall.labelNamespace.enabled=false",
+      "-n #{TESTSUITE_NAMESPACE}",
+    ]
+    helm_install_args_list.push("--set psp.enabled=false") if !version_less_than(KubectlClient.server_version, "1.25.0")
+    helm_install_args = helm_install_args_list.join(" ")
+
+    begin
+      Helm.helm_repo_add("gatekeeper", GATEKEEPER_REPO)
+    rescue ex : Helm::ShellCMD::HelmCMDException
+      logger.error { "Error while installing OPA Gatekeeper: #{ex.message}" }
+      stdout_failure(failed_msg)
+      next
+    end
+
+    begin
+      # (rafal-lal) TODO: should we ensure the release names to be specific to testsuite, imagine sitation in which user already
+      # has gatekeepr installed, will that be a problem?
+      Helm.install("opa-gatekeeper", "gatekeeper/gatekeeper", values: helm_install_args)
+    rescue e : Helm::CannotReuseReleaseNameError
+      logger.notice { "OPA Gatekeeper already installed on the cluster" }
+    rescue ex : Helm::ShellCMD::HelmCMDException
+      logger.error { "Error while installing OPA Gatekeeper: #{ex.message}" }
+      stdout_failure(failed_msg)
+      next
+    end
+    logger.debug { "OPA Gatekeeper chart applied" }
+
+    begin
+      KubectlClient::Wait.wait_for_install_by_apply("./embedded_files/constraint_template.yml")
+      KubectlClient::Wait.wait_for_condition("crd", "requiretags.constraints.gatekeeper.sh", "condition=established", 300)
+      KubectlClient::Apply.file("./embedded_files/enforce-image-tag.yml")
+    rescue ex : KubectlClient::ShellCMD::K8sClientCMDException
+      logger.error { "Error while installing OPA Gatekeeper: #{ex.message}" }
+      stdout_failure(failed_msg)
+      next
+    end
+
+    logger.info { "OPA Gatekeeper tool has been installed" }
   end
 
-  helm_install_args = helm_install_args_list.join(" ")
+  desc "Uninstall OPA"
+  task "uninstall_opa" do |_, args|
+    logger = SLOG.for("uninstall_opa")
+    logger.info { "Uninstalling Open Policy Agent tool" }
+    failed_msg = "Task 'uninstall_opa' failed"
 
-  Helm.helm_repo_add("gatekeeper", "https://open-policy-agent.github.io/gatekeeper/charts")
-  begin
-    Helm.install("opa-gatekeeper", "gatekeeper/gatekeeper", values: helm_install_args)
-  rescue e : Helm::CannotReuseReleaseNameError
-    stdout_warning "gatekeeper already installed"
+    begin
+      Helm.uninstall("opa-gatekeeper", TESTSUITE_NAMESPACE)
+    rescue Helm::ShellCMD::ReleaseNotFound
+    rescue ex : Helm::ShellCMD::HelmCMDException
+      logger.error { "Error while uninstalling OPA Gatekeeper: #{ex.message}" }
+      stdout_failure(failed_msg)
+    end
+
+    begin
+      KubectlClient::Delete.file("./embedded_files/enforce-image-tag.yml")
+    rescue KubectlClient::ShellCMD::NotFoundError
+    rescue ex : KubectlClient::ShellCMD::K8sClientCMDException
+      logger.error { "Error while uninstalling OPA Gatekeeper: #{ex.message}" }
+      stdout_failure(failed_msg)
+    end
+
+    begin
+      KubectlClient::Delete.file("./embedded_files/constraint_template.yml")
+    rescue KubectlClient::ShellCMD::NotFoundError
+    rescue ex : KubectlClient::ShellCMD::K8sClientCMDException
+      logger.error { "Error while uninstalling OPA Gatekeeper: #{ex.message}" }
+      stdout_failure(failed_msg)
+      next
+    end
+
+    logger.info { "Open Policy Agent tool has been uninstalled" }
   end
-
-  File.write("enforce-image-tag.yml", ENFORCE_IMAGE_TAG)
-  File.write("constraint_template.yml", CONSTRAINT_TEMPLATE)
-  KubectlClient::Wait.wait_for_install_by_apply("constraint_template.yml")
-  KubectlClient::Wait.wait_for_condition("crd", "requiretags.constraints.gatekeeper.sh", "condition=established", 300)
-  KubectlClient::Apply.file("enforce-image-tag.yml")
-end
-
-desc "Uninstall OPA"
-task "uninstall_opa" do |_, args|
-  Log.debug { "uninstall_opa" }
-  begin Helm.uninstall("opa-gatekeeper", TESTSUITE_NAMESPACE) rescue Helm::ShellCMD::ReleaseNotFound end
-  begin KubectlClient::Delete.file("enforce-image-tag.yml") rescue KubectlClient::ShellCMD::NotFoundError end
-  begin KubectlClient::Delete.file("constraint_template.yml") rescue KubectlClient::ShellCMD::NotFoundError end
 end
