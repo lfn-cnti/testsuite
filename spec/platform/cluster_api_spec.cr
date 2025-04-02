acf2afc2017b540f8d200e84261a507023110123
Rafal Lal <rafal.lal@tietoevry.com>
2025-04-02 14:33:26 +0000

    Refactor Cluster API setup and change logic of its testcase
    - Refactor logs with usage of .for(), make them more informative
    - Add error handling
    - Clean the code
    - Change logic of testcase to look if cluster on which CNF is running
      was provisioned by Cluster API and not if it has Cluster API manager
      installed
    - Update spec for it
    - Run autoformatter
    
    Signed-off-by: Rafal Lal <rafal.lal@tietoevry.com>

--- spec/platform/cluster_api_spec.cr
+++ spec/platform/cluster_api_spec.cr
@@ -1,38 +1,49 @@
 require "./../spec_helper"
-require "colorize"
 require "file_utils"
 require "../../src/tasks/utils/utils.cr"
 
-describe "Cluster API" do
-  before_all do
-    result = ShellCmd.run_testsuite("setup")
-    result[:status].success?.should be_true
-  end
-  after_each do
-    result = ShellCmd.run_testsuite("setup:cluster_api_uninstall")
-    result[:status].success?.should be_true
-  end
-
-  it "'clusterapi_enabled' should pass if cluster api is installed", tags: ["cluster-api"] do
-    begin
-      result = ShellCmd.run_testsuite("setup:cluster_api_install")
-      current_dir = FileUtils.pwd 
-      FileUtils.cd("#{current_dir}")
-      result = ShellCmd.run_testsuite("clusterapi_enabled poc")
-      (/Cluster API is enabled/ =~ result[:output]).should_not be_nil
-    ensure
-      Log.info { "Running Cleanup" }
-      result = ShellCmd.run_testsuite("setup:cluster_api_uninstall") 
+describe "Platform" do
+  describe "cluster_api_enabled", tags: ["cluster-api"] do
+    before_all do
+      result = ShellCmd.run_testsuite("setup")
+      result[:status].success?.should be_true
     end
-  end
-  
-  it "'clusterapi_enabled' should fail if cluster api is not installed", tags: ["cluster-api-fail"] do
-    begin
-      result = ShellCmd.run_testsuite("clusterapi_enabled poc")
-      (/Cluster API NOT enabled/ =~ result[:output]).should_not be_nil
+
+    # (rafal-lal) TODO: decide how to proceed with Cluster API test case
+    # after_each do
+    #   result = ShellCmd.run_testsuite("uninstall_cluster_api")
+    #   result[:status].success?.should be_true
+    # end
+
+    it "should pass if nodes are managed by Cluster API" do
+      begin
+        node_name = KubectlClient::Get.resource("nodes").dig("items").as_a.first.dig("metadata", "name").as_s
+        KubectlClient::Utils.annotate("node", node_name, ["cluster.x-k8s.io/owner-name=test-cluster-quickstart",
+                                                          "cluster.x-k8s.io/machine=test-1",
+                                                          "cluster.x-k8s.io/cluster-name=test-cluster"])
+      rescue ex : Exception
+        ex.message.should be_nil
+      end
+      result = ShellCmd.run_testsuite("cluster_api_enabled poc")
+      (/At least one node in the cluster is managed by Cluster API/ =~ result[:output]).should_not be_nil
     ensure
-      Log.info { "Running Cleanup" }
-      result = ShellCmd.run_testsuite("setup:cluster_api_uninstall")
+      # (rafal-lal) TODO: decide how to proceed with Cluster API test case
+      # result = ShellCmd.run_testsuite("uninstall_cluster_api")
+      begin
+        node_name = KubectlClient::Get.resource("nodes").dig("items").as_a.first.dig("metadata", "name").as_s
+        KubectlClient::Utils.annotate("node", node_name, ["cluster.x-k8s.io/owner-name-",
+                                                          "cluster.x-k8s.io/machine-",
+                                                          "cluster.x-k8s.io/cluster-name-"])
+      rescue Exception
+      end
+    end
+
+    it "should fail if nodes are not managed by Cluster API" do
+      result = ShellCmd.run_testsuite("cluster_api_enabled poc")
+      (/No nodes in the cluster are managed by Cluster API/ =~ result[:output]).should_not be_nil
+    ensure
+      # (rafal-lal) TODO: decide how to proceed with Cluster API test case
+      # result = ShellCmd.run_testsuite("uninstall_cluster_api")
     end
   end
 end
