fdff7cfb06e0fe6babbade38262c9755f8525799
Rafal Lal <rafal.lal@tietoevry.com>
2025-04-10 12:41:21 +0000

    Refactor helmenv setup task and related helm module utils
    Signed-off-by: Rafal Lal <rafal.lal@tietoevry.com>

---
+++ src/tasks/setup/prereqs.cr
@@ -0,0 +1,25 @@
+require "sam"
+require "file_utils"
+require "colorize"
+require "totem"
+require "../modules/helm"
+
+namespace "setup" do
+  task "prereqs" do |_, args|
+    helm_ok = !Helm::Binary.get.empty? && begin
+      warning, error = Helm.helm_gives_k8s_warning?
+      stdout_failure(error) if !error.nil?
+      !warning
+    end
+    kubectl_ok = KubectlClient.installation_found?
+    git_ok = GitClient.installation_found?
+
+    if [helm_ok, kubectl_ok, git_ok].includes?(false)
+      stdout_failure "Dependency installation failed. Some prerequisites are missing. Please install all of the " +
+                     "prerequisites before continuing."
+      exit(1)
+    else
+      stdout_success "All prerequisites found."
+    end
+  end
+end
