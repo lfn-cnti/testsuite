959d33f636868902f81f831ebc1710dee8c9306a
RobinPopov <robin.popov@tieto.com>
2026-05-20 15:02:26 +0200

    add nightly free5gc workflow and create free5gc spec test (#2381)
    * add nightly free5gc workflow and create free5gc spec test
    
    Introducing free5GC workflow that run CNTi spec tests on free5GC on daily basis.

---
+++ spec/free5gc_cert_spec.cr
@@ -0,0 +1,29 @@
+require "./spec_helper"
+
+describe "Free5gc certification" do
+  before_all do
+    result = ShellCmd.run_testsuite("setup")
+  end
+
+  it "should successfully install and pass certification tests for Free5gc", tags: ["free5gc"] do
+    begin
+      # Install Free5gc
+      ShellCmd.cnf_install("cnf-config=./example-cnfs/free5gc/cnf-testsuite.yml timeout=1800")
+      
+      result = ShellCmd.run_testsuite("cert")
+      
+      # Ensure the testsuite binary didn't crash
+      result[:status].success?.should be_true
+      
+      result[:output].should match(/PASSED/)
+      result[:output].should match(/(17|18|19) of 19 total tests passed/)
+      
+    ensure
+      result = ShellCmd.cnf_uninstall()
+    end
+  end
+
+  after_all do
+    result = ShellCmd.run_testsuite("uninstall_all")
+  end
+end
