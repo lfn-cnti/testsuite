3c2e9faddab77e7bd9c89edb0d1318f146b36313
panagiotisevaggelou <panagiotisevaggelou02@gmail.com>
2026-08-21 12:46:39 +0300

    Format release_manager.cr with crystal tool format

--- src/modules/release_manager/release_manager.cr
+++ src/modules/release_manager/release_manager.cr
@@ -4,10 +4,9 @@ require "log"
 require "mime"
 require "uri"
 
-
 ROOT_URL = "https://api.github.com/repos"
 
-module ReleaseManager 
+module ReleaseManager
   module CompileTimeVersionGenerater
     macro tagged_version
       {% current_branch = `git rev-parse --abbrev-ref HEAD`.split("\n")[0].strip %}
@@ -23,7 +22,7 @@ module ReleaseManager
       {% end %}
     end
   end
-  
+
   CompileTimeVersionGenerater.tagged_version
 
   class GithubReleaseManager
@@ -36,27 +35,27 @@ module ReleaseManager
     end
 
     def github_releases : Array(JSON::Any)
-      existing_releases = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}").
-        get(
+      existing_releases = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}")
+        .get(
           "#{self.repo_url}/releases",
           headers: {Accept: "application/vnd.github.v3+json"}
         )
       JSON.parse(existing_releases.body).as_a
-    end 
+    end
 
     def remote_main_branch_hash
-      results =  `git ls-remote https://github.com/#{@repo_name}.git main | awk '{ print $1}' | cut -c1-7`.strip
-      Log.info {"remote_main_branch_hash: #{results}"}
+      results = `git ls-remote https://github.com/#{@repo_name}.git main | awk '{ print $1}' | cut -c1-7`.strip
+      Log.info { "remote_main_branch_hash: #{results}" }
       results.strip("\n")
     end
 
-    def upsert_release(version=nil) : Tuple((JSON::Any | Nil), (JSON::Any | Nil))
-      Log.info {"upsert_release"}
+    def upsert_release(version = nil) : Tuple((JSON::Any | Nil), (JSON::Any | Nil))
+      Log.info { "upsert_release" }
       found_release : (JSON::Any | Nil) = nil
       asset : (JSON::Any | Nil) = nil
-      Log.info {"version: #{version}"}
+      Log.info { "version: #{version}" }
       upsert_version = (version || ReleaseManager::VERSION)
-      Log.info {"upsert_version: #{upsert_version}"}
+      Log.info { "upsert_version: #{upsert_version}" }
       # cnf_bin_path = "cnf-testsuite"
       # cnf_bin_asset_name = "#{cnf_bin_path}"
       cnf_bin_asset_name = "cnf-testsuite"
@@ -71,19 +70,19 @@ module ReleaseManager
         prerelease = false
         draft = false
       end
-      Log.info {"upsert_version: #{upsert_version}"}
-      Log.info {"upsert_version comparison: upsert_version =~ /(?i)(main|v[0-9]|test_version)/ : #{upsert_version =~ /(?i)(main|v[0-9]|test_version)/}"}
-      #master-381d20d
+      Log.info { "upsert_version: #{upsert_version}" }
+      Log.info { "upsert_version comparison: upsert_version =~ /(?i)(main|v[0-9]|test_version)/ : #{upsert_version =~ /(?i)(main|v[0-9]|test_version)/}" }
+      # master-381d20d
       invalid_version = !(upsert_version =~ /(?i)(main|v[0-9]|test_version)/)
       snap_shot_version = (upsert_version =~ /(?i)(main-)/)
       head = (ReleaseManager.current_branch == "HEAD")
       skip_snapshot_detached_head = (head && snap_shot_version)
-      Log.info {"invalid_version: #{invalid_version}"}
-      Log.info {"current_branch: #{ReleaseManager.current_branch}"}
-      Log.info {"skip_snapshot_detached_head: #{skip_snapshot_detached_head}"}
+      Log.info { "invalid_version: #{invalid_version}" }
+      Log.info { "current_branch: #{ReleaseManager.current_branch}" }
+      Log.info { "skip_snapshot_detached_head: #{skip_snapshot_detached_head}" }
       if skip_snapshot_detached_head || invalid_version
-        Log.info {"Not creating a release for : #{upsert_version}"}
-        return {found_release, asset} 
+        Log.info { "Not creating a release for : #{upsert_version}" }
+        return {found_release, asset}
       end
 
       # NOTE: build MUST be done first so we can sha256sum for release notes
@@ -99,50 +98,50 @@ module ReleaseManager
         # TODO if upsert version == test then make unique
         cnf_tarball_name = "cnf-testsuite-#{upsert_version}.tar.gz"
         cnf_tarball = `tar -czvf #{cnf_tarball_name} ./#{cnf_bin_asset_name}`
-        Log.info {"cnf_tarball: #{cnf_tarball}"}
+        Log.info { "cnf_tarball: #{cnf_tarball}" }
         # cnf_bin_asset_name = "#{cnf_bin_path}-static" # change upload name for static builds
         cnf_bin_asset_name = "#{cnf_tarball_name}" # change upload name for static builds
       end
-      Log.info {"upsert_version: #{upsert_version}"}
+      Log.info { "upsert_version: #{upsert_version}" }
       release_resp = self.github_releases
-      Log.info {"release_resp size: #{release_resp.size}"}
+      Log.info { "release_resp size: #{release_resp.size}" }
 
-      found_release = release_resp.find {|x| x["tag_name"] == upsert_version} 
-      Log.info {"find found_release?: #{found_release}"}
+      found_release = release_resp.find { |x| x["tag_name"] == upsert_version }
+      Log.info { "find found_release?: #{found_release}" }
 
       release_url = "#{self.repo_url}/releases"
       unless found_release
         headers = {Accept: "application/vnd.github.v3+json"}
-        json = { "tag_name" => upsert_version,
-                 "draft" => draft,
-                 "prerelease" => prerelease,
-                 "name" => "#{upsert_version} #{Time.local.to_s("%B %d, %Y")}",
-                 "generate_release_notes" => true }
+        json = {"tag_name"               => upsert_version,
+                "draft"                  => draft,
+                "prerelease"             => prerelease,
+                "name"                   => "#{upsert_version} #{Time.local.to_s("%B %d, %Y")}",
+                "generate_release_notes" => true}
 
-        Log.info {"Release not found.  Creating a release: # url: #{release_url} headers: #{headers} json #{json}"}
+        Log.info { "Release not found.  Creating a release: # url: #{release_url} headers: #{headers} json #{json}" }
 
         found_resp = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}").post(release_url, headers: headers, json: json)
         found_release = JSON.parse(found_resp.body)
         # TODO error if cant create a release
-        Log.info {"(unless) found_release: #{found_release}"}
+        Log.info { "(unless) found_release: #{found_release}" }
       end
 
       # PATCH /repos/:owner/:repo/releases/:release_id
-      found_resp = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}").
-        patch("#{release_url}/#{found_release["id"]}",
-              json: { "tag_name" => upsert_version,
-                      "draft" => draft,
-                      "prerelease" => prerelease,
-                      "name" => "#{upsert_version} #{Time.local.to_s("%B %d, %Y")}",
-                      "generate_release_notes" => true })
+      found_resp = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}")
+        .patch("#{release_url}/#{found_release["id"]}",
+          json: {"tag_name"               => upsert_version,
+                 "draft"                  => draft,
+                 "prerelease"             => prerelease,
+                 "name"                   => "#{upsert_version} #{Time.local.to_s("%B %d, %Y")}",
+                 "generate_release_notes" => true})
       found_release = JSON.parse(found_resp.body)
 
-      Log.info {"found_release (after create): #{found_release}"}
+      Log.info { "found_release (after create): #{found_release}" }
 
-      upload_url = found_release["upload_url"]?.try(&.as_s) || 
-      raise "Release payload is missing 'upload_url'. Response was: #{found_release}"
-    
-      Log.info {"uploading binary"}
+      upload_url = found_release["upload_url"]?.try(&.as_s) ||
+                   raise "Release payload is missing 'upload_url'. Response was: #{found_release}"
+
+      Log.info { "uploading binary" }
       asset = ReleaseManager.upload_release_asset(upload_url, cnf_bin_asset_name)
       {found_release, asset}
     end
@@ -152,26 +151,24 @@ module ReleaseManager
       # DELETE /repos/:owner/:repo/releases/:release_id
       release_resp = self.github_releases
       puts "this is the version #{version}"
-      found_release = release_resp.find {|x| x["tag_name"] == "#{version}"} 
+      found_release = release_resp.find { |x| x["tag_name"] == "#{version}" }
       puts "this is found_release #{typeof(found_release)}"
       if found_release
         puts "this is found_release id #{found_release["id"]}"
-        resp = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}").
-          delete("#{self.repo_url}/releases/#{found_release["id"]}")
+        resp = Halite.auth("Bearer #{ENV["GITHUB_TOKEN"]}")
+          .delete("#{self.repo_url}/releases/#{found_release["id"]}")
         resp_code = resp.status_code
-        Log.info {"resp_code: #{resp_code}"}
-      else 
+        Log.info { "resp_code: #{resp_code}" }
+      else
         resp_code = 404
-      end 
+      end
       resp_code
-    end 
-
-
+    end
   end
 
-  def self.tag(options="")
+  def self.tag(options = "")
     results = `git tag #{options}`
-    Log.info {"git tag: #{results}"}
+    Log.info { "git tag: #{results}" }
     results.split("\n")
   end
 
@@ -181,13 +178,13 @@ module ReleaseManager
 
   def self.current_branch
     results = `git rev-parse --abbrev-ref HEAD`.split("\n")[0].strip
-    Log.info {"current_branch rev-parse: #{results}"}
+    Log.info { "current_branch rev-parse: #{results}" }
     results.strip("\n")
   end
 
   def self.current_hash
     results = `git rev-parse --short HEAD`
-    Log.info {"current_hash rev-parse: #{results}"}
+    Log.info { "current_hash rev-parse: #{results}" }
     results.strip("\n")
   end
 
@@ -219,8 +216,7 @@ module ReleaseManager
     end
 
     asset = JSON.parse(response.body)
-    Log.info {"asset: #{asset}"}
+    Log.info { "asset: #{asset}" }
     asset
   end
-
-end 
+end
