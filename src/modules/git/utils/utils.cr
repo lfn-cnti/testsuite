f0794d5c6ad7a176e0a3021fb9aa973f5c1bb039
viyoma <viyoma.sachdeva@gmail.com>
2026-09-25 13:47:51 -0400

    Update utils.cr

--- src/modules/git/utils/utils.cr
+++ src/modules/git/utils/utils.cr
@@ -4,20 +4,23 @@ require "log"
 require "file_utils"
 require "../constants.cr"
 
+# These mirror the top-level helpers in tasks/utils/utils.cr and honor the same
+# `--output json` gate, so the git setup checks never write to stdout in JSON
+# mode (stdout is reserved for the single results document).
 def stdout_info(msg)
-  puts msg
+  (json_output? ? STDERR : STDOUT).puts msg
 end
 
 def stdout_success(msg)
-  puts msg.colorize(:green)
+  json_output? ? STDERR.puts(msg) : STDOUT.puts(msg.colorize(:green))
 end
 
 def stdout_warning(msg)
-  puts msg.colorize(:yellow)
+  json_output? ? STDERR.puts(msg) : STDOUT.puts(msg.colorize(:yellow))
 end
 
 def stdout_failure(msg)
-  puts msg.colorize(:red)
+  json_output? ? STDERR.puts(msg) : STDOUT.puts(msg.colorize(:red))
 end
 
 def local_git_path
